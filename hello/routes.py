d6770ad234645ef811c359f865a770e3e18312af
Jose Chavez <jrchavez@my.waketech.edu>
2023-02-28 07:13:39 -0500

    Initial commit

---
+++ hello/routes.py
@@ -0,0 +1,6 @@
+from hello import app
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
---
+++ tests/test_client.py
@@ -0,0 +1,6 @@
+from hello import app
+
+def test_route_hello():
+    response = app.test_client().get('/')
+    assert response.data == b'<p>Hello, World!</p>'
+    assert response.status_code == 200
