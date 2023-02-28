f6c97bd7c72e841d5b3f11dcac0e1908e10767b9
Jose Chavez <jrchavez@my.waketech.edu>
2023-02-28 09:30:48 -0500

    update

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,4 +3,4 @@ from hello import app
 def test_route_hello():
     response = app.test_client().get('/')
     assert response.data == b'<p>Hello, World!</p>'
-    assert response.status_code == 210
+    assert response.status_code == 200
